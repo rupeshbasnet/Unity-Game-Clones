b78fd045a1b57558b1f8f39f688f3c74b6e2106c
Rupesh Basnet <rupeshbasnet93@gmail.com>
2018-01-04 17:48:23 -0500

    Added models from brackeys and a fruit script for testing destroy og the object

--- Fruit-Ninja/Assets/Blade.cs
+++ Fruit-Ninja/Assets/Blade.cs
@@ -7,9 +7,14 @@ public class Blade : MonoBehaviour {
 	// Reference our blade trail prefab
 	public GameObject bladeTrailPrefab;
 
-	// We need to have a variable that needs to determine if we are currently
-	// cutting or not
+	public float minCuttingVelocity = 0.001f;
+
+	/* We need to have a variable that needs to determine if we are currently
+	   cutting or not*/
+	
 	bool isCutting = false;
+
+	Vector2 previousPosition;
 
 	GameObject currentBladeTrail;
 
@@ -17,9 +22,12 @@ public class Blade : MonoBehaviour {
 	Rigidbody2D rb;
 	Camera cam;
 
+	CircleCollider2D circleCollider;
+
 	void Start () {
 		cam = Camera.main;
 		rb = GetComponent<Rigidbody2D> ();	
+		circleCollider = GetComponent<CircleCollider2D> ();
 	}
 
 
@@ -34,8 +42,9 @@ public class Blade : MonoBehaviour {
 			StopCutting ();
 		}
 
-		// We need to update the position of our blade to follow our mouse around
-		// whenever we are actually cutting
+		/* We need to update the position of our blade to follow our mouse around
+		   whenever we are actually cutting */
+		
 		if (isCutting) {
 			UpdateCut ();
 		}
@@ -43,33 +52,56 @@ public class Blade : MonoBehaviour {
 	}
 
 	void UpdateCut () {
-		// We want to update the position of our rigidbody which is our blade
-		// Input.mousePosition is the currently screen co-ordinates of our
-		// mouse value. Which is very large. We want to convert this in to 
-		// world co-ordinates, but inorder to convert to the world co-ordinates
-		// we need a reference to our cam which will be in the start method
-		// rb.position = Input.mousePosition;
-		rb.position = cam.ScreenToWorldPoint(Input.mousePosition);
+		 /* 
+		 We want to update the position of our rigidbody which is our blade
+		 Input.mousePosition is the currently screen co-ordinates of our
+		 mouse value. Which is very large. We want to convert this in to 
+		 world co-ordinates, but inorder to convert to the world co-ordinates
+		 we need a reference to our cam which will be in the start method
+		 rb.position = Input.mousePosition; */
 
+		Vector2 newPosition = cam.ScreenToWorldPoint (Input.mousePosition);
+		rb.position = newPosition;
 
+		/* Here we are getting the vector of previous position and the new position; then getting
+		   the velocity from the regular velocity formula */
+		   
+		float velocity = (newPosition - previousPosition).magnitude * Time.deltaTime;
+	
+		if (velocity > minCuttingVelocity) {
+			// Then slice
+			circleCollider.enabled = true;
+		} else {
+			circleCollider.enabled = false;
+		}
+
+		previousPosition = newPosition;
 	}
 
 	void StartCutting () {
 
 		isCutting = true;
-
-		// Everytime we are cutting we wnat to instanciate the bladetrail prefab
-		// However with instantiate we need to supply the cordinates of the parent
-		// game object transform (the location to instantiate the blade trail)
-		// Instantiate(bladeTrailPrefab, transform);
-		// But this will create our instant as a child object
-		// We should unparent it after we instantiate ASAP. therefore we create a reference 
-		// of this instantiated gameobject - > on StopCutting call
-		// We set the parent of this gameobject to null thus deparenting it
-
-		// *** We are parenting the currentBladeTrail b/c we want to trail to be at 
-		// mouse position when it is instantiated ***
+		/*
+		 Everytime we are cutting we wnat to instanciate the bladetrail prefab
+		 However with instantiate we need to supply the cordinates of the parent
+		 game object transform (the location to instantiate the blade trail)
+		 Instantiate(bladeTrailPrefab, transform);
+		 But this will create our instant as a child object
+		 We should unparent it after we instantiate ASAP. therefore we create a reference 
+		 of this instantiated gameobject - > on StopCutting call
+		 We set the parent of this gameobject to null thus deparenting it */
+	  	/* 
+		We are parenting the currentBladeTrail b/c we want to trail to be at 
+		mouse position when it is instantiated */
 		currentBladeTrail = Instantiate(bladeTrailPrefab, transform);
+
+		/* 
+		Here we are resetting the previous position of the blade to the current mouse position */
+
+		previousPosition = cam.ScreenToWorldPoint (Input.mousePosition);
+
+		// We want the circle collider to be enabled when we start cutting 
+		circleCollider.enabled = false;
 
 	}
 
@@ -78,7 +110,12 @@ public class Blade : MonoBehaviour {
 		// This will deparent immediately
 		currentBladeTrail.transform.SetParent (null);
 		Destroy (currentBladeTrail, 2F);
+
+		// enable circle collider clicking
+		circleCollider.enabled = false;
 	}
+
+
 
 
 }
---
+++ Fruit-Ninja/Assets/Fruit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour {
+
+	// Here we want a method that is invoked whenever we detect something triggering and entering
+	// We are using the default unity method for 2D
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		// If the tag of the 2D element that we collide to is Blade tag
+		if (col.tag == "Blade") 
+		{
+			// Test 
+			Debug.Log("We Hit a fruit");
+			// Then we destroy gameobject - > fruit
+			Destroy(gameObject);
+		}
+	}
+}
