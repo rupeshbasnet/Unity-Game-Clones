b4079be9e719164edc10b0c6e0473aa5c5054694
Rupesh Basnet <rupeshbasnet93@gmail.com>
2018-01-04 19:35:57 -0500

    Added the spawn points

--- Fruit-Ninja/Assets/Fruit.cs
+++ Fruit-Ninja/Assets/Fruit.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Fruit : MonoBehaviour {
 
+	// In order to have a slicing effect we will be simply creating a gameobject and
+	// instantiating the slice with some physics
+	public GameObject fruitSlicedPrefab;
+
+
 	// Here we want a method that is invoked whenever we detect something triggering and entering
 	// We are using the default unity method for 2D
 	void OnTriggerEnter2D (Collider2D col)
@@ -11,8 +16,19 @@ public class Fruit : MonoBehaviour {
 		// If the tag of the 2D element that we collide to is Blade tag
 		if (col.tag == "Blade") 
 		{
-			// Test 
-			Debug.Log("We Hit a fruit");
+			// before we destroy the gameobject we will be instantiating the sliced prefab
+			// Also instantiate at the current position and rotation of the unsliced water melon
+
+			// Rotation of the sliced fruit
+			// The collider col is the current position of our mouse. We can get the direction 
+			// of our slice from the col and rotate the slided fruit w.r.t. to the direction of the cut.
+
+			Vector3 direction = (col.transform.position - transform.position).normalized;
+
+			Quaternion rotation = Quaternion.LookRotation (direction);
+
+			Instantiate(fruitSlicedPrefab, transform.position, rotation);
+
 			// Then we destroy gameobject - > fruit
 			Destroy(gameObject);
 		}
---
+++ Fruit-Ninja/Assets/FruitSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour {
+
+	// Here we have a gameobject fruitprefab
+	public GameObject fruitPrefab;
+
+	// Array of transform that has our spawnpoints
+	// To make it more dynamic we can later add random point position and locations 
+	// but for this MVP we only have three static points
+	public Transform[] spawnPoints;
+
+	// Delay max and min for the Wairforseconds method
+	public float minDelay = .1f;
+	public float maxDelay = 1f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (SpawnFruits ());
+	}
+	
+	IEnumerator SpawnFruits ()
+	{
+		while (true) 
+		{	
+			// Add delay in each of these iterations
+			// The delay time could be static. however to make it interesting 
+			float delay = Random.Range(minDelay, maxDelay);
+			yield return new WaitForSeconds(delay);
+
+
+			// Spawn fruits at a random spawn point
+			int spawnIndex = Random.Range(0, spawnPoints.Length);
+			Transform spawnPoint = spawnPoints[spawnIndex];
+
+			Instantiate(fruitPrefab, spawnPoint.position, spawnPoint.rotation);
+		}
+	}
+}
