d94b125d918f12dfc72bccf7f0228d9fb97e9b3b
Rupesh Basnet <rupeshbasnet93@gmail.com>
2018-01-04 22:31:09 -0500

    Base mechanics of the jump and movement

---
+++ Doodle Jump Clone/Assets/Platform.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+	public float jumpForce = 10f;
+
+	void OnCollisionEnter2D(Collision2D col) 
+	{
+		// Check if the object is coming from the buttom or the top
+		// using the relativeVelocity.y to get the velocity of the two components
+		if (col.relativeVelocity.y <= 0f) {
+
+
+			// We need to grab the rigidbody of the thing that collided with the platform
+			// The collider's Get Componenet
+			Rigidbody2D rb = col.collider.GetComponent<Rigidbody2D> ();				
+
+			// However, some elements may not have rigidbody like other platforms
+			if (rb != null) {
+				// We need a way to add the force to the rigidbody that was collided
+				// rb.AddForce ();
+				// Using Addforce will make it such that the rigidbody's current velocity
+				// will compete with the new add force
+
+				// We want to set the velocity directly
+				// We first store the velocity of the rigidbody in its current state
+				Vector2 velocity = rb.velocity;
+				// Set the y attribute of the velocity as the jumpforce
+				velocity.y = jumpForce;
+				// Reset the velocity with out new vector2 velocity
+				rb.velocity = velocity;
+
+
+			}
+		}
+	}
+}
---
+++ Doodle Jump Clone/Assets/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Make sure that we always have a rigidbody2d
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour {
+
+	public float movementSpeed = 10f;
+
+	float movement = 0f;
+
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movement = Input.GetAxis ("Horizontal");
+	}
+
+	// Changing the rigidbody's movement inside of fixed update insteada
+	void FixedUpdate () 
+	{
+		// Get the velocity vector2 of our rigidibody
+		Vector2 velocity = rb.velocity;
+		// Change the velocity's x attr to the movement that we got from the update method
+		velocity.x = movement * movementSpeed;
+		// Setting back the rigidbody's velocity
+		rb.velocity = velocity;
+	}
+}
