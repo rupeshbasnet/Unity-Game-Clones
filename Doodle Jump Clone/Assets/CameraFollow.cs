863771a0e3feb1035c43fd32fd7b2e6534990139
Rupesh Basnet <rupeshbasnet93@gmail.com>
2018-01-04 22:59:19 -0500

    Finished the core machanics with the simple level generator

---
+++ Doodle Jump Clone/Assets/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	// The traget that the camera will follow
+	public Transform target;
+	public float smoothSpeed = .3f;
+	// Update is called once per frame
+	// We want to move the cam behind the player so we use LateUpdate which is 
+	// called after the other updates
+	private Vector3 currentVelocity;
+	void LateUpdate () {
+		// If the target's Y value is greater than our y - value then you want to 
+		// move the camera basically only move with positionve y
+		if (target.position.y > transform.position.y) 
+		{
+			Vector3 newPos = new Vector3 (transform.position.x, 
+				target.position.y, transform.position.z);
+			// Reset the cam's transform position with the y value of the target
+			transform.position = Vector3.SmoothDamp(transform.position, newPos, ref currentVelocity, smoothSpeed * Time.deltaTime);
+		}
+
+	}
+}
---
+++ Doodle Jump Clone/Assets/LevelGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+	// We could use a level generation method of creating levels around the player
+	// However its time consuming for this introduction
+	// We will be loading the entire level as the player starts to play the game
+
+	// Get the reference to the platform
+	public GameObject platformPrefab;
+
+	// Get the int of the number of platforms to spawn
+	public int numberOfPlatforms = 200;
+
+	// The width of our level
+	public float levelWidth = 3f;
+
+	public float minY = .2f;
+	public float maxY = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+
+		Vector3 spawnPosition = new Vector3 ();
+		// Only instantiate till we reach the desired number of platforms
+		for (int i = 0; i < numberOfPlatforms; i++) 
+		{
+			spawnPosition.y += Random.Range (minY, maxY);
+			spawnPosition.x = Random.Range (-levelWidth, levelWidth);
+			// Create the prefab in the spawnPosition and use the Identity for rotation 
+			// which means we wont rotate at all
+			Instantiate (platformPrefab, spawnPosition, Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
